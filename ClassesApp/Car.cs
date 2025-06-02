91ff47d526cf6ae6f6f7de4788d5bde4ed182828
Edon Bytyqi <edonbytyqi@outlook.com>
2025-06-02 09:10:57 +0200

    Add project files.

---
+++ ClassesApp/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesApp
+{
+    internal class Car
+    {
+        //Constructor
+        public Car() 
+        {
+            Console.WriteLine("An Object of Car " +
+                "has been created");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassesApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ClassesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car();
+            Car bmw = new Car();
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
